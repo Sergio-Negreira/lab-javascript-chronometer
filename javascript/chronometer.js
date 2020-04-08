d65279a3d281daa724dc62e89e00bd844d3bed1b
surge0116 <surge0116@gmail.com>
2020-04-08 10:25:52 -0400

    test

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,9 +1,13 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime=0
+    this.intervalId=0
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime+=1
+    })
+    
   }
   getMinutes() {
     // ... your code goes here
